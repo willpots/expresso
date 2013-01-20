7c28525a73a1b1ffd299ffc3fe636a3bf13a2c1c
Will Potter <will@wpotter.me>
2013-01-19 23:22:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msm::Application.config.secret_token = 'a144c39b79e1130b4c4b171a269eee54396d3a6920d71738d950761022608379d7428726e1fc11fe7fb43792967813c66268726b0f333149e1cb7b870c404991'
