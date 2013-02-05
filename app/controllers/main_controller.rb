8dbfcbf46a3582052d75bf9f8f2c36990192ae94
Will Potter <will@wpotter.me>
2013-02-04 23:35:21 -0500

    added git ignore

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,24 +1,26 @@
 class MainController < ApplicationController
   def home
-
+    if session[:valid] == true
+      redirect_to "/orders/new"
+    end
   end
 
   def validate
-  	if params[:email]
-  		@customer = Person.find_by_email(params[:email])
-  		if @customer
-  			session[:email] = params[:email]
-  			session[:valid] = true
-  		end
-  	end
-  	redirect_to "/orders/new"
+    if params[:email]
+      @customer = Person.find_by_email(params[:email])
+      if @customer
+        session[:email] = params[:email]
+        session[:valid] = true
+      end
+    end
+    redirect_to "/orders/new"
   end
 
   def order
- 	if session[:valid] == true
- 		@person = Person.find_by_email session[:email]
-	else
-		redirect_to "/"
-	end
+    if session[:valid] == true
+      @person = Person.find_by_email session[:email]
+    else
+      redirect_to "/"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130205031415) do
+ActiveRecord::Schema.define(:version => 20130205042654) do
 
   create_table "orders", :force => true do |t|
     t.integer  "person_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20130205031415) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "active"
   end
 
 end
