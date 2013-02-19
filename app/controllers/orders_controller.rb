c2309fd9ed1b8df544ea2a9e4913150e42315685
Will Potter <will@wpotter.me>
2013-02-19 16:39:04 -0500

    bunch of changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = Order.all.where(:served => true)
+    @orders = Order.all
 
     respond_to do |format|
       format.html # index.html.erb
